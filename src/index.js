2c015d71092efdb4a6fc7defba06fe7d536a67b1
Akruti <akrutipradhan596@gmail.com>
2026-02-26 09:33:00 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Temp() {
+  const [rating, setRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setRating} />
+      <p>given {rating}</p>
+    </div>
+  );
+}
